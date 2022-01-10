031453ac6f1bedaf468885aed8a97062c123457a
Jason Hsu <jhsu802701@jasonhsu.com>
2022-01-10 22:45:12 +0000

    Initialize repository

---
+++ bundle_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo '-------------------------------------------------------'
+echo "bundle _2.2.17_ config set --local without 'production'"
+bundle _2.2.17_ config set --local without 'production'
+
+echo '-----------------------'
+echo 'bundle _2.2.17_ install'
+bundle _2.2.17_ install
